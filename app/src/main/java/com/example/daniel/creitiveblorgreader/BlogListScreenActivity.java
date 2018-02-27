34409920813a8da82d7588d4e669300aa41e4671
Daniel Stankovic <danielstankovic1992@gmail.com>
2018-02-27 18:40:34 +0100

    doInBackground method done in BlogListScreenActivity. Array of JSONs ready for extracting individual parts.

--- app/src/main/java/com/example/daniel/creitiveblorgreader/BlogListScreenActivity.java
+++ app/src/main/java/com/example/daniel/creitiveblorgreader/BlogListScreenActivity.java
@@ -1,17 +1,26 @@
 package com.example.daniel.creitiveblorgreader;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Base64;
 
 public class BlogListScreenActivity extends AppCompatActivity {
 
@@ -32,6 +41,8 @@ public class BlogListScreenActivity extends AppCompatActivity {
             actionBar.setTitle("Blog List");
         }
 
+        new DownloadTask().execute();
+
         ListView listView = findViewById(R.id.listView);
 
 
@@ -42,13 +53,32 @@ public class BlogListScreenActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
-            String result = "";
+
             try {
 
+                Intent intent = getIntent();
+                String token = intent.getStringExtra("token_value");
                 URL url = new URL("http://blogsdemo.creitiveapps.com/blogs");
 
                 httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.setRequestProperty("Host", "blogsdemo.creitiveapps.com");
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+                httpURLConnection.setRequestProperty("X-Authorize", token);
+                httpURLConnection.setDoInput(true);
 
+                BufferedReader in=new BufferedReader(
+                        new InputStreamReader(
+                                httpURLConnection.getInputStream()));
+                StringBuilder sb = new StringBuilder("");
+                String line="";
+
+                while((line = in.readLine()) != null) {
+                    sb.append(line);
+                }
+
+                in.close();
+                return sb.toString();
 
 
             } catch (MalformedURLException e) {
@@ -58,8 +88,9 @@ public class BlogListScreenActivity extends AppCompatActivity {
             } catch (IOException e) {
 
                 e.printStackTrace();
+            }  finally {
+                httpURLConnection.disconnect();
             }
-
 
             return null;
         }
