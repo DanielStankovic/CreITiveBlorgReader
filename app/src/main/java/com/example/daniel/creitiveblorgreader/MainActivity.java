1a72e34ab650bfd2d77eba9b7d0623c21e892730
Daniel Stankovic <danielstankovic1992@gmail.com>
2018-02-27 12:07:35 +0100

    Added verification for email and password EditText fields

--- app/src/main/java/com/example/daniel/creitiveblorgreader/MainActivity.java
+++ app/src/main/java/com/example/daniel/creitiveblorgreader/MainActivity.java
@@ -2,8 +2,12 @@ package com.example.daniel.creitiveblorgreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailEditText;
@@ -23,7 +27,30 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginUser(View view){
 
+        String email = emailEditText.getText().toString();
+        if(!isEmailValid(email)){
+            emailEditText.setError("Invalid Email");
+        }
+        String password = passwordEditText.getText().toString();
+
+        if(!isPasswordValid(password)){
+            passwordEditText.setError("Invalid password. Password should be longer than 6 characters");
+        }
 
 
+    }
+
+    private boolean isEmailValid(String email){
+
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+
+    }
+
+    private boolean isPasswordValid(String password){
+
+        return password != null && password.length() > 6;
     }
 }
