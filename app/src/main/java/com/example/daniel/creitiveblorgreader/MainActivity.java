046104bb25dd5ff3d532b4f18dd723f809a9b38a
Daniel Stankovic <danielstankovic1992@gmail.com>
2018-02-27 15:07:32 +0100

    Finished doInBackground method that sends the POST request to the server. *Handling Error messages and Exceptions still not done.

--- app/src/main/java/com/example/daniel/creitiveblorgreader/MainActivity.java
+++ app/src/main/java/com/example/daniel/creitiveblorgreader/MainActivity.java
@@ -2,20 +2,43 @@ package com.example.daniel.creitiveblorgreader;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
     EditText emailEditText;
     EditText passwordEditText;
+    String email;
+    String password;
+
+    HttpURLConnection httpURLConnection;
 
 
     @Override
@@ -31,23 +54,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void loginUser(View view){
 
-        String email = emailEditText.getText().toString();
+         email = emailEditText.getText().toString();
         if(!isEmailValid(email)){
             emailEditText.setError("Invalid Email");
         }
-        String password = passwordEditText.getText().toString();
+         password = passwordEditText.getText().toString();
 
         if(!isPasswordValid(password)){
             passwordEditText.setError("Invalid password. Password should be longer than 6 characters");
         }
 
-        checkInternetConnection();
+        if(email.matches("candidate@creitive.com") && password.equals("1234567")) {
+
+           if(checkInternetConnection()){
+               new SendPostRequest().execute();
+           }
+        }
+
 
     }
 
     private boolean isEmailValid(String email){
 
-        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]+$";
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
@@ -59,14 +88,15 @@ public class MainActivity extends AppCompatActivity {
         return password != null && password.length() > 6;
     }
 
-    private void checkInternetConnection(){
+    private boolean checkInternetConnection(){
 
         if(InternetStatus.getInstance(getApplicationContext()).isOnline()){
+            return true;
 
-            //send POST Request and open new Activity
         }else{
 
             showEnableInternetDialog();
+            return false;
         }
 
     }
@@ -96,4 +126,92 @@ public class MainActivity extends AppCompatActivity {
         alert.show();
 
     }
+
+    public class SendPostRequest extends AsyncTask<String, Void, String>{
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            try {
+                URL url = new URL("http://blogsdemo.creitiveapps.com/login");
+
+                JSONObject postParameters = new JSONObject();
+
+                postParameters.put("email", email);
+                postParameters.put("password", password);
+
+                httpURLConnection =(HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestProperty("Accept", "application/json");
+                httpURLConnection.setRequestProperty("Content-Type", "application/json");
+                httpURLConnection.setRequestProperty("Host", "blogsdemo.creitiveapps.com");
+                httpURLConnection.setReadTimeout(15000);
+                httpURLConnection.setConnectTimeout(15000);
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoInput(true);
+                httpURLConnection.setDoOutput(true);
+
+
+                OutputStream os = httpURLConnection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(postParameters.toString());
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode = httpURLConnection.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(
+                            new InputStreamReader(
+                                    httpURLConnection.getInputStream()));
+                    StringBuilder sb = new StringBuilder("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+
+                    }
+
+                    in.close();
+                    Log.i("RESPONSE", sb.toString());
+                    return sb.toString(); //ovde vraca TOKEN
+
+                }
+                else {
+                    Log.i("RESPONSE", String.valueOf(responseCode));
+                    return "false : "+responseCode;
+
+                }
+
+
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
+
+            } catch (JSONException e) {
+
+                e.printStackTrace();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            } finally {
+                httpURLConnection.disconnect();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+
+        }
+    }
+
+   
 }
