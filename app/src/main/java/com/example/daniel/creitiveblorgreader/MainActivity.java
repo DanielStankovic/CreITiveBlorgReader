9df3ec2742735de136177fd79a3f6e5e6a995798
Daniel Stankovic <danielstankovic1992@gmail.com>
2018-02-27 10:26:45 +0100

    Finished Login page layout. Referenced EditText fields from the MainActivity.java

--- app/src/main/java/com/example/daniel/creitiveblorgreader/MainActivity.java
+++ app/src/main/java/com/example/daniel/creitiveblorgreader/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.daniel.creitiveblorgreader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText emailEditText;
+    EditText passwordEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+
+
+    }
+
+    public void loginUser(View view){
+
+
+
     }
 }
